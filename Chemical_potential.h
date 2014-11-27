c05c5ab5666969e20cdabc6bc29f98cc435e3113
Chun Shen <chunshen1987@gmail.com>
2014-11-27 08:22:51 -0500

    add Chemical_potential.h

---
+++ Chemical_potential.h
@@ -0,0 +1,30 @@
+#ifndef CHEMICAL_POTENTIAL_H
+#define CHEMICAL_POTENTIAL_H
+
+#include "Table2D.h"
+
+class Chemical_potential
+{
+   private: 
+      int Tb_length, Nstable;
+      Table2D* EOS_Mu_Table_ptr;
+      double* T;
+      double** mu_table;
+      
+   public:
+      Chemical_potential();
+      ~Chemical_potential();
+
+      void readin_chempotential_table(string filename);
+      void Set_chemical_potential();
+
+      double get_Tblength() {return(Tb_length);};
+      double get_Nstable() {return(Nstable);};
+      double get_T(int i) {return(T[i]);};
+
+      void output_stable_mu(double Temperature, double* mu); 
+
+};
+
+
+#endif
